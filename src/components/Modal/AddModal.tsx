3217520486bb3ff343826077f78a293a5c7b616c
Karol Kawski <karol.kawski@protonmail.com>
2024-03-17 11:10:20 +0100

    [update] orm upgrade

--- src/components/Modal/AddModal.tsx
+++ src/components/Modal/AddModal.tsx
@@ -1,9 +1,8 @@
-import { Button, Modal } from 'flowbite-react';
+import { Modal } from 'flowbite-react';
 import React from 'react';
 import TaskForm from '../Forms/TaskForm';
 import { v4 as uuidv4 } from 'uuid';
 import { AddModalProps } from '../../types/Modal.d';
-import { ButtonTheme } from '../../Themes/ButtonTheme';
 
 const AddModal: React.FC<AddModalProps> = ({
   openModal,
@@ -13,33 +12,20 @@ const AddModal: React.FC<AddModalProps> = ({
   return (
     <Modal dismissible show={openModal} onClose={() => setOpenModal(false)}>
       <Modal.Header>Add task</Modal.Header>
-      <Modal.Body>
-        <TaskForm
-          id={uuidv4()}
-          name={''}
-          description={''}
-          startTime={''}
-          endTime={''}
-          category={''}
-          color={''}
-          priority={''}
-          icon={''}
-          tags={[]}
-          handleAddTask={handleAddTask}
-        />
-      </Modal.Body>
-      <Modal.Footer>
-        <Button
-          className="mr-2 text-black"
-          theme={ButtonTheme}
-          color=""
-          onClick={() => {
-            setOpenModal(false);
-          }}
-        >
-          Cancel
-        </Button>
-      </Modal.Footer>
+      <TaskForm
+        id={uuidv4()}
+        name={''}
+        description={''}
+        startTime={''}
+        endTime={''}
+        category={''}
+        color={''}
+        priority={''}
+        icon={''}
+        tags={[]}
+        setOpenModal={setOpenModal}
+        handleAddTask={handleAddTask}
+      />
     </Modal>
   );
 };
